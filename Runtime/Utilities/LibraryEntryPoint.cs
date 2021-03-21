b3e85a952edbda1af438fd25664f67a2b7ab7d9d
OopsItsACoder <oopsitsacoder@proton.me>
2021-03-21 13:05:33 -0700

    Adding a new attribute to denote library entry points, and fixing up constructors in DIBinder to be more clear

---
+++ Runtime/Utilities/LibraryEntryPoint.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2021 OopsItsACoder
+
+using System;
+using JetBrains.Annotations;
+
+namespace Uject.Utilities
+{
+    [MeansImplicitUse]
+    public class LibraryEntryPoint : Attribute
+    {
+        
+    }
+}
